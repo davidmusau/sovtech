3519f0a4e6c57f3e64b196fd8716c3f7612bc4c9
david_mbuvi <davidmbuvi@icloud.com>
2022-03-28 00:34:46 +0300

    fixed server

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const startServer = async  () =>{
     app.use(cors({origin:"*"}))
     app.use(express.static(path.join(__dirname, '../build')));
 
-    app.get('/', function (req, res) {
+    app.get('/home', function (req, res) {
         res.sendFile(path.join(__dirname+'../build', 'index.html'));
     });
 
