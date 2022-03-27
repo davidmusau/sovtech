ef73702d8238b42ecbcc738cf34c0f9ae273f053
david_mbuvi <davidmbuvi@icloud.com>
2022-03-28 00:32:41 +0300

    fixed server

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const startServer = async  () =>{
     app.use(express.static(path.join(__dirname, '../build')));
 
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+        res.sendFile(path.join(__dirname+'../build', 'index.html'));
     });
 
     const httpServer = createServer(app)
